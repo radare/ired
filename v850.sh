cfc87ed8b7649fdca1ac58f203e9df987daa7d5c
Roman Valls Guimera <brainstorm@nopcode.org>
2020-10-03 09:26:47 +1000

    Toolchain does not do ftruncate and friends. ired compiles now for v850, thx @trufae

--- v850.sh
+++ v850.sh
@@ -4,4 +4,4 @@ CC="docker run --rm --volume $(pwd):$(pwd) -w $(pwd) dkulacz/gcc-v850-elf-toolch
 TOOLCHAIN_PREFIX="v850-elf-"
 FLAGS="-mv850e3v5 -mloop -mrh850-abi"
 $CC ${TOOLCHAIN_PREFIX}gcc ${FLAGS} ired.c -o ired_v850
-$CC ${TOOLCHAIN_PREFIX}size --format=berkeley -o ired_v850.elf
+#$CC ${TOOLCHAIN_PREFIX}size --format=berkeley -o ired_v850.elf
